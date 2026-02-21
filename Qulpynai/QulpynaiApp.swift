7acaebf6baaf6b571c40a7742a7782e4072224f0
fise13 <victhewise@icloud.com>
2026-02-21 15:58:21 +0500

    Initial Commit

---
+++ Qulpynai/QulpynaiApp.swift
@@ -0,0 +1,21 @@
+//
+//  QulpynaiApp.swift
+//  Qulpynai
+//
+//  Created by Виктор on 21.02.2026.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct QulpynaiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
